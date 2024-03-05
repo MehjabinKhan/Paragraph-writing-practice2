f4a2804cddfebffeb7b36414cacf4816faf6b35e
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-05 23:20:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var Eduction = ("Eduction is the important thing in our life.It improves our skill,knowledge and develops the overall personality.It brings great change in our life.It plays major role in women impowerment.It is important for economis growth of nation.It saves you from being fooled or cheated.Eduction is the key of success. ");
+console.log(Eduction);
